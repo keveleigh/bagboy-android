46e190dcec895ca44a7b7557f0a63736c8644f19
jtrujillo91 <trujillj@gmail.com>
2013-12-02 16:09:38 -0500

    Update ListManager.java
    added products depending on category selected

--- src/com/baggers/bagboy/ListManager.java
+++ src/com/baggers/bagboy/ListManager.java
@@ -113,9 +113,268 @@ public class ListManager {
 	public static ArrayList<String> loadItemsFromCategory (String categoryName) {
 		//return db.loadItemsFromCategory(categoryName);
 		ArrayList<String> items = new ArrayList<String>();
-		//a bunch of if statements for the different categories
-		//need to look at database for this info
-		
+		if (categories.equal("Wine"){
+			items.add("Cabernet");
+			items.add("White Wine");
+			items.add("Dessert Wine");
+			items.add("Red Wine");
+			items.add("Chardonnay");
+			items.add("Zinfandel");
+		}
+		else if (categories.equal("Bread"){
+			items.add("Buns");
+			items.add("Rolls");
+			items.add("Bread");
+			items.add("Hamburger");
+			items.add("French");
+			items.add("Italian");
+			items.add("Wheat");
+			items.add("White");
+		}
+		else if (categories.equal("Baked Goods"){
+			items.add("Biscuits");
+		}
+		else if (categories.equal("Beer"){
+			
+		}
+		else if (categories.equal("Bakery"){
+			items.add("Croissants");
+			items.add("Cakes");
+			items.add("Donuts");
+		}
+		else if (categories.equal("Deli"){
+			
+		}
+		else if (categories.equal("Syrup"){
+			
+		}
+		else if (categories.equal("Peanut Butter"){
+			
+		}
+		else if (categories.equal("Cereals"){
+			
+		}
+		else if (categories.equal("Coffee/Tea"){
+			
+		}
+		else if (categories.equal("Powdered Drinks"){
+			
+		}
+		else if (categories.equal("Spices/Extract"){
+			
+		}
+		else if (categories.equal("Cake Mix"){
+			items.add("Cake Mix");
+			items.add("Brownie Mix");
+			items.add("Pancacke Mix");
+			items.add("Flour");
+		}
+		else if (categories.equal("Sugar"){
+			
+		}
+		else if (categories.equal("Soup"){
+			
+		}
+		else if (categories.equal("Pickles/Olives"){
+			
+		}
+		else if (categories.equal("Canned Veggies"){
+			items.add("Asparagus");
+			items.add("Carrots");
+			items.add("Corn");
+			items.add("Greenbeans");
+			items.add("Peas");
+			items.add("Potatoes");
+			items.add("Tomatoes");
+			
+		}
+		else if (categories.equal("Pasta"){
+			
+		}
+		else if (categories.equal("Spaghetti/Sauces"){
+			
+		}
+		else if (categories.equal("Rice/Dry Beans"){
+			items.add("Baked Beans");
+			items.add("Butter Beans");
+			items.add("Kidney Beans");
+			items.add("Pinto Beans");
+			items.add("String Beans");
+		}
+		else if (categories.equal("Cookies"){
+			
+		}
+		else if (categories.equal("Bathroom Tissue"){
+			
+		}
+		else if (categories.equal("Paper Towels"){
+			
+		}
+		else if (categories.equal("Snacks"){
+			items.add("Candy");
+			items.add("Cookies");
+			items.add("Crackers");
+			items.add("Nuts");
+			items.add("Popcorn");
+			items.add("Potato Chips");
+			items.add("Pretzels");
+			items.add("Raisins");
+		}
+		else if (categories.equal("Cola"){
+			items.add("Generic Brand");
+			items.add("Coca-Cola");
+			items.add("Pepsi");
+		}
+		else if (categories.equal("Potato Chips"){
+			
+		}
+		else if (categories.equal("Pet Food"){
+			
+		}
+		else if (categories.equal("Charcoal"){
+			
+		}
+		else if (categories.equal("Candles"){
+			
+		}
+		else if (categories.equal("Laundry Detergent"){
+			
+		}
+		else if (categories.equal("Dishwashing Detergent"){
+			items.add("Dishwashing Soap");
+			items.add("Sponges");
+		}
+		else if (categories.equal("Medicines"){
+			items.add("Antiacid");
+			items.add("Bandaids");
+			items.add("Cough Drops");
+			items.add("First Aid Cream");
+			items.add("Hydrogen Peroxide");
+			items.add("Pain-Reliever");
+			items.add("Rubbing Alcohol");
+		}
+		else if (categories.equal("Baby Products"){
+			
+		}
+		else if (categories.equal("Deodorant"){
+			
+		}
+		else if (categories.equal("Hair Care"){
+			
+		}
+		else if (categories.equal("Bar Soap"){
+			
+		}
+		else if (categories.equal("Cards"){
+			
+		}
+		else if (categories.equal("Water"){
+			items.add("Bottled Water");
+			items.add("Sparkling Water");
+		}
+		else if (categories.equal("Cheese"){
+			items.add("Boursin");
+			items.add("Brie");
+			items.add("Cheese");
+			items.add("Shredded");
+			items.add("Mozarella");
+			items.add("Cottage Cheese");
+			items.add("Cream Cheese");
+		}
+		else if (categories.equal("Milk"){
+			
+		}
+		else if (categories.equal("Vitamins"){
+			
+		}
+		else if (categories.equal("Frozen Breakfast"){
+			items.add("Breakfast Sausage");
+			items.add("Waffles");
+		}
+		else if (categories.equal("Frozen Meats"){
+			items.add("Chicken Nuggets");
+			items.add("Chicken Breasts");
+			items.add("Chicken Tenderloins");
+			items.add("Ground Beef");
+			items.add("Ground Turkey");
+		}
+		else if (categories.equal("Frozen Juices"){
+			
+		}
+		else if (categories.equal("Bacon"){
+			
+		}
+		else if (categories.equal("Ice Cream"){
+			items.add("Premium Ice Cream");
+			items.add("Simple Ice Cream");
+			items.add("Ice Cream");
+		}
+		else if (categories.equal("Frozen Dessert"){
+			
+		}
+		else if (categories.equal("Frozen Seafood"){
+			items.add("Flounder");
+			items.add("Mahi Mahi");
+			items.add("Tilapia");
+		}
+		else if (categories.equal("Frozen Potatoes"){
+			
+		}
+		else if (categories.equal("Novelties"){
+			
+		}
+		else if (categories.equal("Frozen Dinners"){
+			items.add("Hamburger Helper");
+		}
+		else if (categories.equal("Frozen Veggies"){
+			items.add("Frozen Veggies");
+		}
+		else if (categories.equal("Frozen Entrees"){
+			
+		}
+		else if (categories.equal("Frozen Food"){
+			
+		}
+		else if (categories.equal("Frozen Pizza"){
+			
+		}
+		else if (categories.equal("Fruits"){
+			items.add("Apples");
+			items.add("Avocados");
+			items.add("Bananas");
+			items.add("Berries");
+			items.add("Cherries"):
+			items.add("Grapefruit");
+			items.add("Grapes");
+			items.add("Kiwis");
+			items.add("Lemons/Limes");
+			items.add("Melon");
+			items.add("Oranges"):
+			items.add("Peaches");
+		}
+		else if (categories.equal("Produce"){
+			items.add("Apples");
+			items.add("Avocados");
+			items.add("Bananas");
+			items.add("Berries");
+			items.add("Cherries"):
+			items.add("Grapefruit");
+			items.add("Grapes");
+			items.add("Kiwis");
+			items.add("Lemons/Limes");
+			items.add("Melon");
+			items.add("Oranges"):
+			items.add("Peaches");
+		}
+		else if (categories.equal("Meats"){
+			items.add("Beef");
+			items.add("Chicken");
+			items.add("Fish");
+			items.add("Pork");
+		}
+		else if (categories.equal("Seafood"){
+			
+		}
 		return items;
 	}
 	
