25da10f67f9046d0b8f37b55bcf4d0a83edd753a
Cliff Harris <charr91@gmail.com>
2013-12-02 22:49:53 -0500

    Map stuff

--- src/com/baggers/bagboy/MapActivity.java
+++ src/com/baggers/bagboy/MapActivity.java
@@ -2,6 +2,7 @@ package com.baggers.bagboy;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -11,6 +12,9 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Dialog;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -19,27 +23,65 @@ import android.view.ViewGroup;
 
 public class MapActivity extends Fragment {
 	
-    private GoogleMap map;
+    private GoogleMap gm;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {        
 		// Inflate the layout for this fragment
 		View v = inflater.inflate(R.layout.activity_map, container, false);
 		
-		map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-		
-//		int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
-//		if (status == ConnectionResult.SUCCESS) {
-//	      SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-//	      map = supportMapFragment.getMap();
-//	    }
-//	    else {
-//	      int requestCode = 10;
-//	      Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
-//	      dialog.show();
-//	    }
-		
+		// Checking play services status
+		int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
+		if(status!=ConnectionResult.SUCCESS){ // Google Play Services are not available
+
+	        int requestCode = 10;
+	        Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, getActivity(), requestCode);
+	        dialog.show();
+
+	    } else { // Play services available
+	    	gm = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+			gm.setMyLocationEnabled(true);
+			
+			gm.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(33.7749832,-84.3966354) , 14.0f) );
+			gm.addMarker(new MarkerOptions()
+		     .position(new LatLng(33.7749832,-84.3966354))
+		     .title("Georgia Institute of Technology")
+		     .snippet("4 stores nearby"));
+			
+//			// Getting LocationManager object from System Service LOCATION_SERVICE
+//	        LocationManager locationManager = (LocationManager) getActivity().getSystemService(getActivity().LOCATION_SERVICE);
+//
+//	        // Creating a criteria object to retrieve provider
+//	        Criteria criteria = new Criteria();
+//
+//	        // Getting the name of the best provider
+//	        String provider = locationManager.getBestProvider(criteria, true);
+//
+//	        // Getting Current Location
+//	        Location location = locationManager.getLastKnownLocation(provider);
+//
+//	        LocationListener ll = new LocationListener() {
+//		          public void onLocationChanged(Location location) {
+//		          // redraw the marker when get location update.
+//		          drawMarker(location);
+//		          }
+//	        };
+//
+//	        if(location!=null){
+//	           //PLACE THE INITIAL MARKER              
+//	           drawMarker(location);
+//	        }
+//	        locationManager.requestLocationUpdates(provider, 20000, 0, ll);
+	    }
 	    
 	    return v;
+	}
+	
+	private void drawMarker(Location location){
+		gm.clear();
+		LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+		gm.addMarker(new MarkerOptions()
+			.position(currentPosition)
+			.snippet("Lat:" + location.getLatitude() + "Lng:"+ location.getLongitude()));
 	}
 }
