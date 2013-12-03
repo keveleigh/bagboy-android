2ffc8a7ae197137c80417550059e834f64ee09c1
jtrujillo91 <trujillj@gmail.com>
2013-12-02 19:28:49 -0500

    Update ListManager.java
    made changes to errors

--- src/com/baggers/bagboy/ListManager.java
+++ src/com/baggers/bagboy/ListManager.java
@@ -18,7 +18,7 @@ public class ListManager {
 	
 	public static void createList(String listName) {
 		currProductList = new ArrayList<String>();
-		listCollection.add(currProductList);
+		listCollection.addAll(currProductList);
 		//db.createList(LoginManager.currUserEmail, listName);
 	}
 	
@@ -115,7 +115,7 @@ public class ListManager {
 	public static ArrayList<String> loadItemsFromCategory (String categoryName) {
 		//return db.loadItemsFromCategory(categoryName);
 		ArrayList<String> items = new ArrayList<String>();
-		if (categories.equal("Wine"){
+		if (categoryName.equal("Wine")){
 			items.add("Cabernet");
 			items.add("White Wine");
 			items.add("Dessert Wine");
@@ -123,7 +123,7 @@ public class ListManager {
 			items.add("Chardonnay");
 			items.add("Zinfandel");
 		}
-		else if (categories.equal("Bread"){
+		else if (categoryName.equal("Bread")){
 			items.add("Buns");
 			items.add("Rolls");
 			items.add("Bread");
@@ -133,35 +133,35 @@ public class ListManager {
 			items.add("Wheat");
 			items.add("White");
 		}
-		else if (categories.equal("Baked Goods"){
+		else if (categoryName.equal("Baked Goods")){
 			items.add("Biscuits");
 		}
-		else if (categories.equal("Beer"){
+		else if (categoryName.equal("Beer")){
 			items.add("Budweiser");
 			items.add("Bud Light");
 			items.add("Shock Top");
 			items.add("Blue Moon");
 		}
-		else if (categories.equal("Bakery"){
+		else if (categoryName.equal("Bakery")){
 			items.add("Croissants");
 			items.add("Cakes");
 			items.add("Donuts");
 		}
-		else if (categories.equal("Deli"){
+		else if (categoryName.equal("Deli")){
 			items.add("Deli");
 		}
-		else if (categories.equal("Syrup"){
+		else if (categoryName.equal("Syrup")){
 			items.add("Auntie May");
 			items.add("Cranberry Syrup");
 			items.add("Syrup");
 		}
-		else if (categories.equal("Peanut Butter"){
+		else if (categoryName.equal("Peanut Butter")){
 			items.add("Peanut Butter");
 			items.add("Jif");
 			items.add("Skippy");
 			items.add("Peter Pan");
 		}
-		else if (categories.equal("Cereals"){
+		else if (categoryName.equal("Cereals")){
 			items.add("Cereals");
 			items.add("Apple Jacks");
 			items.add("Frosted Flakes");
@@ -171,19 +171,19 @@ public class ListManager {
 			items.add("Chex");
 			items.add("Cinnamon Toast Crunch");
 		}
-		else if (categories.equal("Coffee/Tea"){
+		else if (categoryName.equal("Coffee/Tea")){
 			items.add("Coffee");
 			items.add("Tea");
 			items.add("Ground Coffee");
 		}
-		else if (categories.equal("Powdered Drinks"){
+		else if (categoryName.equal("Powdered Drinks")){
 			items.add("Crystal Light");
 			items.add("Lipton Tea");
 			items.add("Gatorade");
 			items.add("Nestle Nesquik");
 			items.add("Powerade");
 		}
-		else if (categories.equal("Spices/Extract"){
+		else if (categoryName.equal("Spices/Extract")){
 			items.add("Basil");
 			items.add("Spices");
 			items.add("Extract");
@@ -196,24 +196,24 @@ public class ListManager {
 			items.add("Sage");
 			items.add("Thyme");
 		}
-		else if (categories.equal("Cake Mix"){
+		else if (categoryName.equal("Cake Mix")){
 			items.add("Cake Mix");
 			items.add("Brownie Mix");
 			items.add("Pancacke Mix");
 			items.add("Flour");
 		}
-		else if (categories.equal("Sugar"){
+		else if (categoryName.equal("Sugar")){
 			items.add("Sugar");
 		}
-		else if (categories.equal("Soup"){
+		else if (categoryName.equal("Soup")){
 			items.add("Soup");
 			items.add("Tomato Soup");
 		}
-		else if (categories.equal("Pickles/Olives"){
+		else if (categoryName.equal("Pickles/Olives")){
 			items.add("Pickles");
 			items.add("Olives");
 		}
-		else if (categories.equal("Canned Veggies"){
+		else if (categoryName.equal("Canned Veggies")){
 			items.add("Asparagus");
 			items.add("Carrots");
 			items.add("Corn");
@@ -223,36 +223,36 @@ public class ListManager {
 			items.add("Tomatoes");
 			
 		}
-		else if (categories.equal("Pasta"){
+		else if (categoryName.equal("Pasta")){
 			items.add("Pasta");
 		}
-		else if (categories.equal("Spaghetti/Sauces"){
+		else if (categoryName.equal("Spaghetti/Sauces")){
 			items.add("Spaghetti");
 			items.add("Sauce");
 		}
-		else if (categories.equal("Rice/Dry Beans"){
+		else if (categoryName.equal("Rice/Dry Beans")){
 			items.add("Baked Beans");
 			items.add("Butter Beans");
 			items.add("Kidney Beans");
 			items.add("Pinto Beans");
 			items.add("String Beans");
 		}
-		else if (categories.equal("Cookies"){
+		else if (categoryName.equal("Cookies")){
 			items.add("Cookies");
 			items.add("Sugar Cookies")l
 			items.add("Chocolate Chip Cookies");
 			items.add("Cookie Dough");
 		}
-		else if (categories.equal("Bathroom Tissue"){
+		else if (categoryName.equal("Bathroom Tissue")){
 			items.add("Bathroom Tissue");
 			items.add("Toilet Paper");
 			items.add("Bathroom Cleaners");
 		}
-		else if (categories.equal("Paper Towels"){
+		else if (categoryName.equal("Paper Towels")){
 			items.add("Paper Towels");
 			items.add("Garbage Bags");
 		}
-		else if (categories.equal("Snacks"){
+		else if (categoryName.equal("Snacks")){
 			items.add("Candy");
 			items.add("Cookies");
 			items.add("Crackers");
@@ -262,33 +262,33 @@ public class ListManager {
 			items.add("Pretzels");
 			items.add("Raisins");
 		}
-		else if (categories.equal("Cola"){
+		else if (categoryName.equal("Cola")){
 			items.add("Generic Brand");
 			items.add("Coca-Cola");
 			items.add("Pepsi");
 		}
-		else if (categories.equal("Potato Chips"){
+		else if (categoryName.equal("Potato Chips")){
 			items.add("Chips");
 			items.add("Lays Chips");
 			items.add("Doritos");
 		}
-		else if (categories.equal("Pet Food"){
+		else if (categoryName.equal("Pet Food")){
 			items.add("Ped Food");
 		}
-		else if (categories.equal("Charcoal"){
+		else if (categoryName.equal("Charcoal")){
 			items.add("Charcoal");
 		}
-		else if (categories.equal("Candles"){
+		else if (categoryName.equal("Candles")){
 			items.add("Candles");
 		}
-		else if (categories.equal("Laundry Detergent"){
+		else if (categoryName.equal("Laundry Detergent")){
 			items.add("Laundry Detergent");
 		}
-		else if (categories.equal("Dishwashing Detergent"){
+		else if (categoryName.equal("Dishwashing Detergent")){
 			items.add("Dishwashing Soap");
 			items.add("Sponges");
 		}
-		else if (categories.equal("Medicines"){
+		else if (categoryName.equal("Medicines")){
 			items.add("Antiacid");
 			items.add("Bandaids");
 			items.add("Cough Drops");
@@ -297,30 +297,30 @@ public class ListManager {
 			items.add("Pain-Reliever");
 			items.add("Rubbing Alcohol");
 		}
-		else if (categories.equal("Baby Products"){
+		else if (categoryName.equal("Baby Products")){
 			items.add("Baby Products");
 			items.add("Bottles");
 		}
-		else if (categories.equal("Deodorant"){
+		else if (categoryName.equal("Deodorant")){
 			items.add("Deodorant");
 		}
-		else if (categories.equal("Hair Care"){
+		else if (categoryName.equal("Hair Care")){
 			items.add("Shampoo");
 			items.add("Conditioner");
 		}
-		else if (categories.equal("Bar Soap"){
+		else if (categoryName.equal("Bar Soap")){
 			items.add("Bar Soap");
 			items.add("Soap");
 			items.add("Body Lotion");
 		}
-		else if (categories.equal("Cards"){
+		else if (categoryName.equal("Cards")){
 			items.add("Cards");
 		}
-		else if (categories.equal("Water"){
+		else if (categoryName.equal("Water")){
 			items.add("Bottled Water");
 			items.add("Sparkling Water");
 		}
-		else if (categories.equal("Cheese"){
+		else if (categoryName.equal("Cheese")){
 			items.add("Boursin");
 			items.add("Brie");
 			items.add("Cheese");
@@ -329,77 +329,77 @@ public class ListManager {
 			items.add("Cottage Cheese");
 			items.add("Cream Cheese");
 		}
-		else if (categories.equal("Milk"){
+		else if (categoryName.equal("Milk")){
 			items.add("Milk");
 			items.add("2 Percent Milk");
 			items.add("Skim Milk");
 			items.add("Soy Milk");
 		}
-		else if (categories.equal("Vitamins"){
+		else if (categoryName.equal("Vitamins")){
 			
 		}
-		else if (categories.equal("Frozen Breakfast"){
+		else if (categoryName.equal("Frozen Breakfast")){
 			items.add("Breakfast Sausage");
 			items.add("Waffles");
 		}
-		else if (categories.equal("Frozen Meats"){
+		else if (categoryName.equal("Frozen Meats")){
 			items.add("Chicken Nuggets");
 			items.add("Chicken Breasts");
 			items.add("Chicken Tenderloins");
 			items.add("Ground Beef");
 			items.add("Ground Turkey");
 		}
-		else if (categories.equal("Frozen Juices"){
+		else if (categoryName.equal("Frozen Juices")){
 			items.add("Orange Juice");
 			items.add("Juice");
 			items.add("Apple Juice");
 		}
-		else if (categories.equal("Bacon"){
+		else if (categoryName.equal("Bacon")){
 			items.add("Bacon");
 		}
-		else if (categories.equal("Ice Cream"){
+		else if (categoryName.equal("Ice Cream")){
 			items.add("Premium Ice Cream");
 			items.add("Simple Ice Cream");
 			items.add("Ice Cream");
 		}
-		else if (categories.equal("Frozen Dessert"){
+		else if (categoryName.equal("Frozen Dessert")){
 			items.add("Frozen Dessert");
 			items.add("Pie");
 			items.add("Ice Cream");
 		}
-		else if (categories.equal("Frozen Seafood"){
+		else if (categoryName.equal("Frozen Seafood")){
 			items.add("Flounder");
 			items.add("Mahi Mahi");
 			items.add("Tilapia");
 		}
-		else if (categories.equal("Frozen Potatoes"){
+		else if (categoryName.equal("Frozen Potatoes")){
 			items.add("French Fries");
 			items.add("Frozen Potatoes");
 		}
-		else if (categories.equal("Novelties"){
+		else if (categoryName.equal("Novelties")){
 			items.add("Novelties");
 			items.add("Paper");
 			items.add("Pencils"):
 			items.add("Notebook");
 		}
-		else if (categories.equal("Frozen Dinners"){
+		else if (categoryName.equal("Frozen Dinners")){
 			items.add("Hamburger Helper");
 		}
-		else if (categories.equal("Frozen Veggies"){
+		else if (categoryName.equal("Frozen Veggies")){
 			items.add("Frozen Veggies");
 		}
-		else if (categories.equal("Frozen Entrees"){
+		else if (categoryName.equal("Frozen Entrees")){
 			items.add("Frozen Entrees");
 			items.add("Hot Pockets");
 			items.add("Lean Cuisine");
 		}
-		else if (categories.equal("Frozen Food"){
+		else if (categoryName.equal("Frozen Food")){
 			items.add("Frozen Food");
 		}
-		else if (categories.equal("Frozen Pizza"){
+		else if (categoryName.equal("Frozen Pizza")){
 			items.add("Frozen Pizza");
 		}
-		else if (categories.equal("Fruits"){
+		else if (categoryName.equal("Fruits")){
 			items.add("Apples");
 			items.add("Avocados");
 			items.add("Bananas");
@@ -413,7 +413,7 @@ public class ListManager {
 			items.add("Oranges"):
 			items.add("Peaches");
 		}
-		else if (categories.equal("Produce"){
+		else if (categoryName.equal("Produce")){
 			items.add("Apples");
 			items.add("Avocados");
 			items.add("Bananas");
@@ -427,13 +427,13 @@ public class ListManager {
 			items.add("Oranges"):
 			items.add("Peaches");
 		}
-		else if (categories.equal("Meats"){
+		else if (categoryName.equal("Meats")){
 			items.add("Beef");
 			items.add("Chicken");
 			items.add("Fish");
 			items.add("Pork");
 		}
-		else if (categories.equal("Seafood"){
+		else if (categoryName.equal("Seafood")){
 			items.add("Seafood");	
 		}
 		return items;
